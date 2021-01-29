a33f00f3f3350d2dedff3f5df9e69190a2c0d408
spyros87 <papadopoulos.spyridon@gmail.com>
2021-01-29 20:31:50 +0100

    Implement Part Two of Day one.

--- pkg/day01/report_test.go
+++ pkg/day01/report_test.go
@@ -51,3 +51,59 @@ func Test_searchForTwoEntriesEqualTo(t *testing.T) {
 		})
 	}
 }
+
+func Test_searchForThreeEntriesEqualTo(t *testing.T) {
+	type args struct {
+		expenses    []int
+		expectedSum int
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantFirst  int
+		wantSecond int
+		wantThird  int
+		wantErr    bool
+	}{
+		{
+			name: "Triplet resulting 2020 is found",
+			args: args{
+				expenses:    []int{299, 366, 675, 979, 1456, 1721},
+				expectedSum: 2020,
+			},
+			wantFirst:  366,
+			wantSecond: 675,
+			wantThird:  979,
+			wantErr:    false,
+		},
+		{
+			name: "No triplet resulting 2020 is found",
+			args: args{
+				expenses:    []int{295, 366, 854, 979, 1456, 1721},
+				expectedSum: 2020,
+			},
+			wantFirst:  0,
+			wantSecond: 0,
+			wantThird:  0,
+			wantErr:    true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotFirst, gotSecond, gotThird, err := searchForThreeEntriesEqualTo(tt.args.expenses, tt.args.expectedSum)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("searchForThreeEntriesEqualTo() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotFirst != tt.wantFirst {
+				t.Errorf("searchForThreeEntriesEqualTo() gotFirst = %v, want %v", gotFirst, tt.wantFirst)
+			}
+			if gotSecond != tt.wantSecond {
+				t.Errorf("searchForThreeEntriesEqualTo() gotSecond = %v, want %v", gotSecond, tt.wantSecond)
+			}
+			if gotThird != tt.wantThird {
+				t.Errorf("searchForThreeEntriesEqualTo() gotThird = %v, want %v", gotThird, tt.wantThird)
+			}
+		})
+	}
+}
