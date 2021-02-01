271c8c904a8e6687664484c0dffe19aa77288d74
spyros87 <papadopoulos.spyridon@gmail.com>
2021-02-01 10:58:24 +0100

    Add Day Two  in main.

--- cmd/advent-of-code-2020/main.go
+++ cmd/advent-of-code-2020/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/spyros87/advent-of-code-2020/pkg/day01"
+import (
+	"github.com/spyros87/advent-of-code-2020/pkg/day01"
+	"github.com/spyros87/advent-of-code-2020/pkg/day02"
+)
 
 func main() {
 	day01.ReportRepair()
+	day02.PasswordPhilosophy()
 }
